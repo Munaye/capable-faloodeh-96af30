91ac9c69dd1ce6e0035f4d03063f880d7811fa31
Sharon Okeke <munachio39@gmail.com>
2026-04-20 21:31:20 +0100

    fixed layout issue

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const modal = document.getElementById("contactModal");
 const btn = document.getElementById("hireBtn");
 const closeBtn = document.getElementById("closeModal");
@@ -19,3 +20,30 @@ window.onclick = function(e) {
     modal.style.display = "none";
   }
 }
+=======
+const modal = document.getElementById("contactModal");
+const btn = document.getElementById("hireBtn");
+const closeBtn = document.getElementById("closeModal");
+
+// Open modal when clicking the Hire Me button
+btn.onclick = function(e) {
+  e.preventDefault(); // stops the page from jumping
+  modal.style.display = "block";
+}
+
+// Close modal when clicking the X
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Close modal when clicking outside the modal box
+window.onclick = function(e) {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+function toggleMenu(){
+  document.querySelector('.nav-links').classList.toggle('active');
+}
+>>>>>>> f83c07a (fixed layout issue)
